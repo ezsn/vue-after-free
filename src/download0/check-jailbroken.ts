09c51634259c266ec9eaa9d61ef5b18d6522bb31
ezsn <ezsn@proton.me>
2026-03-04 12:40:21 +0700

    fix auto close already jailbroken

--- src/download0/check-jailbroken.ts
+++ src/download0/check-jailbroken.ts
@@ -24,5 +24,30 @@ export function checkJailbroken (): boolean {
 
   const jailbroken = uidAfterVal === 0
   log(jailbroken ? 'Already jailbroken' : 'Not jailbroken')
+  if(jailbroken){
+    if (typeof CONFIG !== 'undefined' && CONFIG.autoclose && !BinLoader.skip_autoclose) {
+      const closeDelay = (typeof CONFIG !== 'undefined' && CONFIG.autoclose_delay) ? CONFIG.autoclose_delay : 0 // set to 20000 for ps4 hen
+
+      fn.register(0x14, 'getpid', [], 'bigint')
+      fn.register(0x25, 'kill', ['bigint', 'bigint'], 'bigint')
+
+      const pid = fn.getpid()
+      const pid_num = (pid instanceof BigInt) ? pid.lo : pid
+      log('Current PID: ' + pid_num)
+
+      if (closeDelay > 0) {
+        log('CONFIG.autoclose enabled - closed after ' + (closeDelay / 1000) + ' seconds...')
+        utils.notify('Vue closed after ' + (closeDelay / 1000) + ' seconds...')
+        const killId = jsmaf.setInterval(function () {
+          jsmaf.clearInterval(killId)
+          log('Sending SIGKILL to PID ' + pid_num)
+          fn.kill(pid, new BigInt(0, 9))
+        }, closeDelay)
+      } else {
+        log('CONFIG.autoclose enabled - closing now')
+        fn.kill(pid, new BigInt(0, 9))
+      }
+    }
+  }
   return jailbroken
 }
